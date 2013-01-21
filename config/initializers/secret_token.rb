aaad16d97f8f26b29b9bbc623cb1764f922052d5
Denise <dkeathe@sandia.gov>
2013-01-21 16:43:05 -0700

    Generate the Application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ticketee::Application.config.secret_token = 'eff97cf9b68292ab3707a9c8a1aa9156128c64cbc6a64975ccdc84442f283a83855418a07b2a32a22faf59ecadf5568f5c2829509e749b796de0c607efc6dbd3'
