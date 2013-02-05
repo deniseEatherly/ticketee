fa797af030196c2c58ec6f347d95691a5583f6d3
Denise <dkeathe@sandia.gov>
2013-02-05 16:52:47 -0700

    Creation of projects feature complete.

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,23 @@
+class ProjectsController < ApplicationController
+
+  def index
+
+  end
+
+  def new
+     @project = Project.new
+  end
+
+  def create
+     @project = Project.new(params[:project])
+    if @project.save
+       redirect_to @project, :notice  => "Project has been created."
+    else
+
+    end
+  end
+
+  def show
+     @project = Project.find(params[:id])
+  end
+end
